e38e16c6671e1a3b53c4bb6772c88b136222b324
Oleksandr-Kononiuk <morganblack@ukr.net>
2021-10-11 16:12:07 +0300

    implement MealDaoImpl.java

--- src/main/java/ru/javawebinar/topjava/dao/MealDao.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDao.java
@@ -2,10 +2,13 @@ package ru.javawebinar.topjava.dao;
 
 import ru.javawebinar.topjava.model.Meal;
 
+import java.util.List;
+
 public interface MealDao {
 
-    void create(Meal meal);
-    Meal read(long id);
-    Meal update(long id, Meal meal);
+    void add(Meal meal);
+    Meal findById(long id);
+    List<Meal> findAll();
+    void update(long id, Meal meal);
     void delete(long id);
 }
--- src/main/java/ru/javawebinar/topjava/dao/impl/MealDaoImpl.java
+++ src/main/java/ru/javawebinar/topjava/dao/impl/MealDaoImpl.java
@@ -2,26 +2,57 @@ package ru.javawebinar.topjava.dao.impl;
 
 import ru.javawebinar.topjava.dao.MealDao;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class MealDaoImpl implements MealDao {
 
-    @Override
-    public void create(Meal meal) {
+    private List<Meal> meals = MealsUtil.generateMealList();
 
+    @Override
+    public void add(Meal meal) {
+        meals.add(meal);
     }
 
     @Override
-    public Meal read(long id) {
-        return null;
+    public Meal findById(long id) {
+        return meals.stream()
+                .filter((meal) -> meal.getId() == id)
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
     }
 
     @Override
-    public Meal update(long id, Meal meal) {
-        return null;
+    public List<Meal> findAll() {
+        return meals;
+    }
+
+    @Override
+    public void update(long id, Meal meal) {
+        int index = 0;
+        for(int i = 0; i < meals.size(); i++) {
+            if (meals.get(i).getId() == id) {
+                meals.remove(i);
+                index = i;
+                break;
+            }
+        }
+        meals.add(index, meal);
+        //? meals.replaceAll(m -> meal);
     }
 
     @Override
     public void delete(long id) {
+        meals.removeIf((meal -> meal.getId() == id));
+    }
 
+    public List<Meal> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
     }
 }
