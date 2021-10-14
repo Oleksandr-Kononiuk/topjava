6a44c13028286b30f6fe30f5fb742ea95e01da6f
Oleksandr-Kononiuk <morganblack@ukr.net>
2021-10-14 15:55:40 +0300

    implemented concurrent operations on meal collection

--- src/main/java/ru/javawebinar/topjava/dao/impl/MealDaoImpl.java
+++ src/main/java/ru/javawebinar/topjava/dao/impl/MealDaoImpl.java
@@ -6,27 +6,28 @@ import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class MealDaoImpl implements MealDao {
 
-    private List<Meal> meals = MealsUtil.generateMealList();
+    private List<Meal> meals = new CopyOnWriteArrayList<>(MealsUtil.generateMealList());
     private static final AtomicLong COUNTER = new AtomicLong(7); // todo first 7 value is hardcoded in MealUtils
 
     @Override
     public void add(Meal meal) {
-        System.out.println(meal.getId());
         meal.setId(COUNTER.incrementAndGet());
-        System.out.println(meal.getId());
         meals.add(meal);
     }
 
     @Override
     public Meal findById(long id) {
-        return meals.stream()
-                .filter((meal) -> meal.getId() == id)
-                .findFirst()
-                .orElseThrow(NoSuchElementException::new);
+        for (Meal meal : meals) {
+            if (meal.getId() == id) {
+                return meal;
+            }
+        }
+        throw new NoSuchElementException("Meal not found!");
     }
 
     @Override
@@ -36,11 +37,11 @@ public class MealDaoImpl implements MealDao {
 
     @Override
     public void update(long id, Meal meal) {
-        for(int i = 0; i < meals.size(); i++) {
-            if (meals.get(i).getId() == id) {
+        for (Meal m : meals) {
+            if (m.getId() == id) {
                 meal.setId(id);
-                meals.remove(i);
-                meals.add(i, meal);
+                this.delete(id);
+                meals.add(meal);
                 break;
             }
         }
@@ -48,7 +49,10 @@ public class MealDaoImpl implements MealDao {
 
     @Override
     public void delete(long id) {
-        meals.remove(findById(id));
+        meals.forEach(meal -> {
+            if (meal.getId() == id)
+                meals.remove(meal);
+        });
     }
 
     public List<Meal> getMeals() {
