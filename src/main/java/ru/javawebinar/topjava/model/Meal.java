aa040c126fc0cdda0cde01ff6fd893366ed00ac6
Oleksandr-Kononiuk <morganblack@ukr.net>
2021-10-30 13:00:03 +0300

    hibernate mapping Meal.java

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,16 +1,30 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+
+    @Column(name = "date_time", unique = true, nullable = false)
+    @NotNull
     private LocalDateTime dateTime;
 
+    @Column(name = "description", nullable = false)
+    @Size(min = 2, max = 100, message = "Minimal length must be 2 character, maximum - 100.")
+    @NotBlank
     private String description;
 
+    @Column(name = "calories", nullable = false)
+    @Range(min = 10, max = 5000, message = "Minimal value must be 10, maximum - 5000.")
+    @NotNull
+    @Positive
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
