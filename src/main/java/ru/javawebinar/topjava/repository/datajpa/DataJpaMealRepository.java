24ffdfe40b76c3e5c70d8eff41361afd46f9b208
Oleksandr-Kononiuk <morganblack@ukr.net>
2021-11-07 15:57:49 +0300

    Implemented datajpa tests, abstract class for service tests

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepository.java
@@ -1,43 +1,78 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.AbstractBaseEntity;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.DateTimeUtil;
+import ru.javawebinar.topjava.util.Util;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
+@Transactional
 public class DataJpaMealRepository implements MealRepository {
 
-    private final CrudMealRepository crudRepository;
+    private final CrudMealRepository mealRepository;
+    private final CrudUserRepository userRepository;
 
-    public DataJpaMealRepository(CrudMealRepository crudRepository) {
-        this.crudRepository = crudRepository;
+    public DataJpaMealRepository(CrudMealRepository crudRepository, CrudUserRepository userRepository) {
+        this.mealRepository = crudRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        meal.setUser(userRepository.getById(userId));
+        if (meal.isNew()) {
+            return mealRepository.save(meal);
+        } else if (get(meal.id(), userId) == null ){
+            return null;
+        }
+        return mealRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        Meal meal = get(id, userId);
+        if (meal != null) {
+            mealRepository.delete(meal);
+            return true;
+        } else {
+            return false;
+        }
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Meal get(int id, int userId) {
-        return null;
+        return getAll(userId).stream()
+                .filter(meal -> meal.id() == id)
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Meal> getAll(int userId) {
-        return null;
+       return mealRepository.findAll()
+                .stream()
+                .filter(meal -> meal.getUser().id() == userId)
+                .sorted(Collections.reverseOrder(Comparator.comparingInt(AbstractBaseEntity::getId)))
+                .collect(Collectors.toList());
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return null;
+        return getAll(userId).stream()
+                .filter(meal -> Util.isBetweenHalfOpen(meal.getDateTime(), startDateTime, endDateTime))
+                .collect(Collectors.toList());
     }
 }
