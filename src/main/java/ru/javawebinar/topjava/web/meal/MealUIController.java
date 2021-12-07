fb395cacc1fdcc578cf5a8f9d17600f8bf0e9bf4
Oleksandr-Kononiuk <morganblack@ukr.net>
2021-12-07 12:45:44 +0300

    completed 1

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.lang.Nullable;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealTo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/meals", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MealUIController extends AbstractMealController {
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        super.delete(id);
+    }
+
+    @PostMapping(value = "/update/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable int id, @RequestBody Meal meal) {
+        super.update(meal, id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public @ResponseBody Meal create(@RequestParam Map<String, String> params) {
+        Meal meal = new Meal(
+                LocalDateTime.parse(params.get("dateTime")),
+                params.get("description"),
+                Integer.parseInt(params.get("calories")));
+        return super.create(meal);
+    }
+
+    @Override
+    @GetMapping("/filter")
+    public List<MealTo> getBetween(
+            @RequestParam(name = "startDate") @Nullable LocalDate startDate,
+            @RequestParam(name = "startTime") @Nullable LocalTime startTime,
+            @RequestParam(name = "endDate") @Nullable LocalDate endDate,
+            @RequestParam(name = "endTime") @Nullable LocalTime endTime) {
+        return super.getBetween(startDate, startTime, endDate, endTime);
+    }
+
+    @Override
+    @GetMapping
+    public List<MealTo> getAll() {
+        return super.getAll();
+    }
+}
